f5c9d26487e3ee75c999f91f1016785a8092d54a
Jimmy Israkhan <jimmy.israkhan@gmail.com>
2021-08-24 18:52:46 +0700

    Changed CORS middleware usage.

--- src/index.ts
+++ src/index.ts
@@ -14,9 +14,9 @@ dotenv.config();
 const PORT = process.env.PORT || 3000;
 const app: Express = express();
 
-app.use(cors());
 app.use(compression());
-// app.use(cors(corsConfig([`http://localhost:${PORT}`])));
+// app.use(cors());
+app.use(cors(corsConfig([`http://localhost:${PORT}`])));
 app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
